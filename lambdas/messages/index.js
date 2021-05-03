d92723734931fbb13914146748622f55bfb46837
ArmandoMota <armandomotagt@gmail.com>
2021-05-02 18:37:51 -0700

    Added resend message functionality with SNS
    Co-authored-by: Juan Palma <juanpedropalma@hotmail.com>
    Co-authored-by: Kevin Counihan <kevin.counihan17@gmail.com>
    Co-authored-by: Kayl Thomas <kaylthomas607@gmail.com>

--- lambdas/messages/index.js
+++ lambdas/messages/index.js
@@ -3,9 +3,13 @@ const { getMessage, resendMessage, listMessages } = require("./messageActions");
 
 exports.handler = async (event) => {
   let { pathParameters, httpMethod, resource } = event;
-  const serviceId = pathParameters ? pathParameters.service_id : pathParameters;
-  const userId = pathParameters ? pathParameters.user_id : pathParameters;
-  const messageId = pathParameters ? pathParameters.message_id : pathParameters;
+  const serviceUuid = pathParameters
+    ? pathParameters.service_id
+    : pathParameters;
+  const userUuid = pathParameters ? pathParameters.user_id : pathParameters;
+  const messageUuid = pathParameters
+    ? pathParameters.message_id
+    : pathParameters;
   const isResend =
     resource ===
     "/services/{service_id}/users/{user_id}/messages/{message_id}/resend";
@@ -14,12 +18,12 @@ exports.handler = async (event) => {
 
   if (isResend && httpMethod === "POST") {
     console.log("Resending message");
-    return await resendMessage(messageId);
-  } else if (messageId && httpMethod === "GET") {
+    return await resendMessage(messageUuid);
+  } else if (messageUuid && httpMethod === "GET") {
     console.log("Getting message");
-    return await getMessage(messageId);
+    return await getMessage(messageUuid);
   } else if (httpMethod === "GET") {
     console.log("Listing all messages for user");
-    return await listMessages(userId);
+    return await listMessages(userUuid);
   }
 };
--- lambdas/messages/messageActions.js
+++ lambdas/messages/messageActions.js
@@ -1,21 +1,56 @@
 const { db, serviceUuidToPK } = require("./db");
+const AWS = require("aws-sdk");
+const sns = new AWS.SNS({ apiVersion: "2010-03-31" });
 
-const resendMessage = async (messageId) => {
-  const message = await getMessageToResend(messageId);
+const resendMessage = async (messageUuid) => {
+  const params = await getMessageParams(messageUuid);
 
-  if (message) {
-    // format SNS message object
-
-    try {
-      // add message object to SNS queue
-      return newResponse(202, {});
-    } catch (error) {
-      console.error(error);
-      return newResponse(500, { Error: "Could not resend message" });
-    }
-  } else {
+  if (!params) {
     return newResponse(404, { Error: "Could not find message to resend" });
   }
+
+  try {
+    await sns.publish(params).promise();
+    return newResponse(202, {});
+  } catch (error) {
+    console.error(error);
+    return newResponse(500, { Error: "Could not resend message" });
+  }
+};
+
+const getMessageParams = async (messageUuid) => {
+  const text = `SELECT messages.endpoint, events.payload, event_types.sns_topic_arn
+    FROM messages
+    JOIN events ON messages.event_id = events.id
+    JOIN event_types ON events.event_type_id = event_types.id
+    WHERE messages.uuid = $1`;
+  const values = [messageUuid];
+
+  try {
+    const response = await db.query(text, values);
+    if (!response.rows) return;
+
+    const messageData = response.rows[0];
+    return constructParams(messageData);
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+};
+
+const constructParams = (messageData) => {
+  const { endpoint, payload, sns_topic_arn } = messageData;
+  return {
+    TopicArn: sns_topic_arn,
+    Message: JSON.stringify(payload),
+    MessageAttributes: {
+      url: {
+        // need to know what attribute name we're using for this if not url!!!
+        DataType: "String",
+        StringValue: endpoint,
+      },
+    },
+  };
 };
 
 const getMessage = async (messageId) => {
@@ -27,10 +62,10 @@ const getMessage = async (messageId) => {
     const response = await db.query(text, values);
     if (response.rows.length === 0) {
       return newResponse(404, { Error: "Message not found" });
-    } else {
-      let responseBody = response.rows[0];
-      return newResponse(200, responseBody);
     }
+
+    let responseBody = response.rows[0];
+    return newResponse(200, responseBody);
   } catch (error) {
     console.error(error);
     return newResponse(500, { Error: "Could not get message" });
@@ -40,39 +75,26 @@ const getMessage = async (messageId) => {
 const listMessages = async (userId) => {
   userId = await serviceUuidToPK(userId);
 
-  if (userId) {
-    const text = `SELECT messages.uuid, event_id, endpoint, delivery_attempts, delivered_at
-      FROM messages
-      JOIN events ON messages.event_id = events.id
-      JOIN users ON events.user_id = users.id
-      WHERE users.id = $1`;
-    const values = [userId];
-
-    try {
-      const response = await db.query(text, values);
-      let responseBody = response.rows;
-      return newResponse(200, responseBody);
-    } catch (error) {
-      console.error(error);
-      return newResponse(500, { Error: "Could not get messages" });
-    }
-  } else {
+  if (!userId) {
     return newResponse(404, {
       Error: "Could not find user associated to message",
     });
   }
-};
 
-const getMessageToResend = async (messageId) => {
-  const text = "SELECT * FROM messages WHERE uuid = $1";
-  const values = [messageId];
+  const text = `SELECT messages.uuid, event_id, endpoint, delivery_attempts, delivered_at
+    FROM messages
+    JOIN events ON messages.event_id = events.id
+    JOIN users ON events.user_id = users.id
+    WHERE users.id = $1`;
+  const values = [userId];
 
   try {
     const response = await db.query(text, values);
-    return response.rows[0];
+    let responseBody = response.rows;
+    return newResponse(200, responseBody);
   } catch (error) {
     console.error(error);
-    return;
+    return newResponse(500, { Error: "Could not get messages" });
   }
 };
 
