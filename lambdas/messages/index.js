7a0969eeb6c6dc4950ee5ff88369cfd0d3a01188
ArmandoMota <armandomotagt@gmail.com>
2021-05-04 13:17:46 -0700

    Cleaned up error messaging for messages lambda
    Co-authored-by: Juan Palma <juanpedropalma@hotmail.com>
    Co-authored-by: Kevin Counihan <kevin.counihan17@gmail.com>
    Co-authored-by: Kayl Thomas <kaylthomas607@gmail.com>

--- lambdas/messages/index.js
+++ lambdas/messages/index.js
@@ -1,29 +1,39 @@
 "use strict";
 const { getMessage, resendMessage, listMessages } = require("./messageActions");
+const { newResponse, isValidService, isValidUser } = require("./utils");
 
 exports.handler = async (event) => {
   let { pathParameters, httpMethod, resource } = event;
-  const serviceUuid = pathParameters
-    ? pathParameters.service_id
-    : pathParameters;
-  const userUuid = pathParameters ? pathParameters.user_id : pathParameters;
-  const messageUuid = pathParameters
-    ? pathParameters.message_id
-    : pathParameters;
   const isResend =
     resource ===
     "/services/{service_id}/users/{user_id}/messages/{message_id}/resend";
+  const messageUuid = pathParameters
+    ? pathParameters.message_id
+    : pathParameters;
 
-  // do we need to validate whether serviceId and userId are valid here?
+  const userUuid = pathParameters ? pathParameters.user_id : pathParameters;
+  if (!(await isValidUser(userUuid))) {
+    return newResponse(404, {
+      error_Type: "data_not_found",
+      detail: "No user matches given uuid.",
+    });
+  }
+
+  const serviceUuid = pathParameters
+    ? pathParameters.service_id
+    : pathParameters;
+  if (!(await isValidService(serviceUuid))) {
+    return newResponse(404, {
+      error_Type: "data_not_found",
+      detail: "No service matches given uuid.",
+    });
+  }
 
   if (isResend && httpMethod === "POST") {
-    console.log("Resending message");
     return await resendMessage(messageUuid, serviceUuid);
   } else if (messageUuid && httpMethod === "GET") {
-    console.log("Getting message");
     return await getMessage(messageUuid);
   } else if (httpMethod === "GET") {
-    console.log("Listing all messages for user");
     return await listMessages(userUuid);
   }
 };
