7e31724820e11348d46e7250fc8def707999ebff
ArmandoMota <armandomotagt@gmail.com>
2021-05-24 12:27:06 -0700

    Updated uuidToId bug and added event_type to get and list message return values

--- lambdas/messages/db.js
+++ lambdas/messages/db.js
@@ -20,15 +20,18 @@ const queries = {
     FROM messages
     JOIN events ON messages.event_id = events.id
     WHERE messages.uuid = $1 AND messages.deleted_at = '${nullTimeValue}'`,
-  getMessage: `SELECT uuid, endpoint, delivery_attempt, status_code, created_at, delivered
-    FROM messages
-    WHERE uuid = $1 AND deleted_at = '${nullTimeValue}'`,
-  listMessages: `SELECT messages.uuid, events.uuid AS event_id, messages.endpoint, delivery_attempt, status_code, messages.created_at, delivered
-    FROM messages
-    JOIN events ON messages.event_id = events.id
-    JOIN users ON events.user_id = users.id
-    WHERE users.uuid = $1 AND messages.deleted_at = '${nullTimeValue}'
-    ORDER BY messages.created_at DESC`,
+  getMessage: `SELECT m.uuid, m.endpoint, et.name AS event_type, m.delivery_attempt, m.status_code, m.created_at, m.delivered
+    FROM messages m
+    JOIN events e ON m.event_id = e.id
+    JOIN event_types et ON e.event_type_id = et.id 
+    WHERE m.uuid = $1 AND m.deleted_at = '${nullTimeValue}'`,
+  listMessages: `SELECT m.uuid, e.uuid AS event_id, et.name AS event_type, m.endpoint, delivery_attempt, status_code, m.created_at, delivered
+    FROM messages m
+    JOIN events e ON m.event_id = e.id
+    JOIN event_types et ON e.event_type_id = et.id
+    JOIN users u ON e.user_id = u.id
+    WHERE u.uuid = $1 AND m.deleted_at = '${nullTimeValue}'
+    ORDER BY m.created_at DESC`,
 };
 
 module.exports.db = pool;
