762f6579560bd903699ad405c0748245cb838bf4
ArmandoMota <armandomotagt@gmail.com>
2021-06-01 11:42:52 -0700

    Updating stats lambda to accomodate updated event type and endpoint routes

--- lambdas/stats/db.js
+++ lambdas/stats/db.js
@@ -1,5 +1,5 @@
-const { Pool } = require("pg");
-
+const nullTimeValue = '1970-01-01T00:00:00.000Z';
+const { Pool } = require('pg');
 const user = process.env.DATABASE_USER;
 const password = process.env.DATABASE_PASSWORD;
 const host = process.env.DATABASE_HOST;
@@ -13,13 +13,7 @@ const db = new Pool({
   port,
 });
 
-const getCount = async (query, queryParams) => {
-  const response = await db.query(query, queryParams);
-  console.log(query, queryParams, response);
-  const count = response.rows[0].count;
-  return count;
-};
-
+// Simple count queries
 const getEndpointCount = async (serviceId) => {
   const query = `SELECT count(*) FROM endpoints
     JOIN users ON endpoints.user_id = users.id
@@ -28,40 +22,56 @@ const getEndpointCount = async (serviceId) => {
   return await getCount(query, queryParams);
 };
 
-const getMessagesByDay = async (serviceId) => {
-  const query = `SELECT count(m.id) as count, date_trunc('day', m.created_at) as day FROM messages m
-    JOIN events e ON e.id = m.event_id
-    JOIN users ON e.user_id = users.id
-    WHERE users.service_id = $1
-    GROUP BY date_trunc('day', m.created_at)`;
-
-  const queryParams = [serviceId];
-  return await getEntities(query, queryParams);
-};
-
-const getMessagesByMonth = async (serviceId) => {
-  const query = `SELECT count(m.id) as count, date_trunc('month', m.created_at) as month FROM messages m
-    JOIN events e ON e.id = m.event_id
-    JOIN users ON e.user_id = users.id
-    WHERE users.service_id = $1
-    GROUP BY date_trunc('month', m.created_at)`;
-  const queryParams = [serviceId];
-  return await getEntities(query, queryParams);
-};
-
-const getEntities = async (query, queryParams) => {
-  const response = await db.query(query, queryParams);
-  console.log(query, queryParams, response);
-  const entities = response.rows;
-  return entities;
-};
-
-const getEvents = async (serviceId) => {
+const getEventCount = async (serviceId) => {
   const query = `SELECT count(*) as count FROM events e
     JOIN users u ON u.id = e.user_id
     WHERE u.service_id = $1`;
   const queryParams = [serviceId];
   return await getCount(query, queryParams);
+};
+
+const getFailedMessageCount = async (serviceId) => {
+  const query = `SELECT count(m.uuid)
+    FROM messages m
+    JOIN events e ON e.id = m.event_id
+    JOIN users u ON u.id = e.user_id
+    WHERE u.service_id = $1 AND m.delivered = 'false'`;
+  const queryParams = [serviceId];
+  return await getCount(query, queryParams);
+};
+
+const getMessageCount = async (serviceId) => {
+  const query = `SELECT count(m.uuid)
+    FROM messages m
+    JOIN events e ON e.id = m.event_id
+    JOIN users u ON u.id = e.user_id
+    WHERE u.service_id = $1`;
+  const queryParams = [serviceId];
+  return await getCount(query, queryParams);
+};
+
+const getUserCount = async (serviceId) => {
+  const query = `SELECT count(*) FROM users
+    WHERE users.service_id = $1`;
+  const queryParams = [serviceId];
+  return await getCount(query, queryParams);
+};
+
+// Compound count queries
+const getMessageCountByEndpoint = async (userId) => {
+  const query = `SELECT e.url, count(e.url) as all,
+    SUM(1) FILTER (WHERE m.delivered = 'false') AS failed
+    FROM endpoints e
+    JOIN messages m ON m.endpoint = e.url
+    WHERE e.user_id = $1 AND e.deleted_at = '${nullTimeValue}'
+    GROUP BY e.url`;
+  const queryParams = [userId];
+  const counts = await getEntities(query, queryParams);
+  return counts.reduce((agg, curr) => {
+    const { url, all, failed } = curr;
+    agg[url] = { all, failed };
+    return agg;
+  }, {});
 };
 
 const getEventsByType = async (serviceId) => {
@@ -70,7 +80,11 @@ const getEventsByType = async (serviceId) => {
     WHERE et.service_id = $1
     GROUP BY et.name`;
   const queryParams = [serviceId];
-  return await getEntities(query, queryParams);
+  const counts = await getEntities(query, queryParams);
+  return counts.reduce((agg, curr) => {
+    agg[curr.type] = curr.count;
+    return agg;
+  }, {});
 };
 
 const getEventsByUser = async (serviceId) => {
@@ -79,23 +93,188 @@ const getEventsByUser = async (serviceId) => {
     WHERE u.service_id = $1
     GROUP BY u.name`;
   const queryParams = [serviceId];
+  const counts = await getEntities(query, queryParams);
+  return counts.reduce((agg, curr) => {
+    agg[curr.user] = curr.count;
+    return agg;
+  }, {});
+};
+
+const getEventTypeCountByEndpoint = async (userId) => {
+  const query = `SELECT e.uuid, e.created_at, e.url, array_agg(et.name) AS event_types
+    FROM endpoints e
+    JOIN subscriptions s ON s.endpoint_id = e.id
+    JOIN event_types et ON et.id = s.event_type_id
+    WHERE e.user_id = $1 AND e.deleted_at = '${nullTimeValue}'
+    GROUP BY e.id`;
+  const queryParams = [userId];
   return await getEntities(query, queryParams);
 };
 
-const getMessages = async (serviceId) => {
-  const query = `SELECT messages.uuid, messages.status_code, messages.created_at, messages.delivered FROM messages
-  JOIN events ON events.id = messages.event_id
-  JOIN users ON events.user_id = users.id
-  WHERE users.service_id = $1`;
-  const queryParams = [serviceId];
-  return await getEntities(query, queryParams);
+const getMessagesByDay = async (serviceId) => {
+  const date = new Date(),
+    y = date.getFullYear(),
+    m = date.getMonth();
+  const firstDay = new Date(y, m, 1);
+  const query = `SELECT count(m.id) as count, date_trunc('day', m.created_at) as day FROM messages m
+    JOIN events e ON e.id = m.event_id
+    JOIN users ON e.user_id = users.id
+    WHERE users.service_id = $1 AND date_trunc('month', m.created_at) = $2
+    GROUP BY date_trunc('day', m.created_at)`;
+  const queryParams = [serviceId, firstDay];
+  const counts = await getEntities(query, queryParams);
+  return counts.reduce((agg, curr) => {
+    agg[curr.day] = curr.count;
+    return agg;
+  }, {});
 };
 
-const getUserCount = async (serviceId) => {
-  const query = `SELECT count(*) FROM users
-    WHERE users.service_id = $1`;
+const getMessagesByMonth = async (serviceId) => {
+  const date = new Date(),
+    y = date.getFullYear();
+  const firstDay = new Date(y, 0, 1);
+  const query = `SELECT count(m.id) as count, date_trunc('month', m.created_at) as month FROM messages m
+    JOIN events e ON e.id = m.event_id
+    JOIN users ON e.user_id = users.id
+    WHERE users.service_id = $1 AND date_trunc('year', m.created_at) = $2
+    GROUP BY date_trunc('month', m.created_at)`;
+  const queryParams = [serviceId, firstDay];
+  const counts = await getEntities(query, queryParams);
+  return counts.reduce((agg, curr) => {
+    agg[curr.month] = curr.count;
+    return agg;
+  }, {});
+};
+
+const getMessagesByStatus = async (serviceId) => {
+  const query = `SELECT count(m.id) as count, m.status_code FROM messages m
+    JOIN events e ON e.id = m.event_id
+    JOIN users u ON u.id = e.user_id
+    WHERE u.service_id = $1
+    GROUP BY m.status_code`;
   const queryParams = [serviceId];
-  return await getCount(query, queryParams);
+  const counts = await getEntities(query, queryParams);
+  return counts.reduce((agg, curr) => {
+    agg[curr.status_code] = curr.count;
+    return agg;
+  }, {});
+};
+
+const getMessagesByYear = async (serviceId) => {
+  const query = `SELECT count(m.id) as count, date_trunc('year', m.created_at) as year FROM messages m
+    JOIN events e ON e.id = m.event_id
+    JOIN users ON e.user_id = users.id
+    WHERE users.service_id = $1
+    GROUP BY date_trunc('year', m.created_at)`;
+  const queryParams = [serviceId];
+  const counts = await getEntities(query, queryParams);
+  return counts.reduce((agg, curr) => {
+    agg[curr.year] = curr.count;
+    return agg;
+  }, {});
+};
+
+const getFirstMessageDate = async (serviceId) => {
+  const query = `SELECT m.created_at FROM messages m
+    JOIN events e ON e.id = m.event_id
+    JOIN users u ON u.id = e.user_id
+    WHERE u.service_id = $1
+    ORDER BY m.created_at
+    LIMIT 1`;
+  const queryParams = [serviceId];
+  const response = await db.query(query, queryParams);
+  const messageData = response.rows;
+  const { created_at } = messageData[0];
+  if (created_at) {
+    const firstDate = new Date(created_at);
+    return {
+      day: firstDate.getDate() - 1,
+      month: firstDate.getMonth(),
+      year: firstDate.getFullYear(),
+    };
+  }
+};
+
+// Utility functions
+const getCount = async (query, queryParams) => {
+  const response = await db.query(query, queryParams);
+  const count = response.rows[0].count;
+  return parseInt(count, 10);
+};
+
+const getEntities = async (query, queryParams) => {
+  const response = await db.query(query, queryParams);
+  let entities = response.rows;
+
+  if (entities[0]) {
+    const { count, subscribers, day, month, year } = entities[0];
+
+    if (count) {
+      entities = entities.map((entity) => {
+        return { ...entity, count: parseInt(entity.count, 10) };
+      });
+    }
+
+    if (subscribers) {
+      entities = entities.map((entity) => {
+        return { ...entity, subscribers: parseInt(entity.subscribers, 10) };
+      });
+    }
+
+    if (day) {
+      entities = entities.map((entity) => {
+        const day = new Date(entity.day).getDate() - 1;
+        return { ...entity, day };
+      });
+    }
+
+    if (month) {
+      entities = entities.map((entity) => {
+        const month = new Date(entity.month).getMonth();
+        return { ...entity, month };
+      });
+    }
+
+    if (year) {
+      entities = entities.map((entity) => {
+        const year = new Date(entity.year).getFullYear();
+        return { ...entity, year };
+      });
+    }
+  }
+
+  return entities;
+};
+
+const getEventTypes = async (serviceId) => {
+  const query = `SELECT et.name, et.created_at, count(s.event_type_id) as subscribers
+    FROM event_types et
+    LEFT JOIN subscriptions s ON s.event_type_id = et.id
+    WHERE et.service_id = $1 AND et.deleted_at = '${nullTimeValue}'
+    GROUP BY et.name, et.created_at, s.event_type_id`;
+  const queryParams = [serviceId];
+  const entities = await getEntities(query, queryParams);
+  return entities;
+};
+
+const getService = async (serviceUuid) => {
+  const query = `SELECT id
+    FROM services
+    WHERE uuid = $1 AND deleted_at = '${nullTimeValue}'`;
+  const queryParams = [serviceUuid];
+  const response = await db.query(query, queryParams);
+  const service = response.rows[0];
+  return service;
+};
+
+const getUser = async (serviceId, userUuid) => {
+  const query = `SELECT id
+    FROM users
+    WHERE service_id = $1 AND uuid = $2 AND deleted_at = '${nullTimeValue}'`;
+  const queryParams = [serviceId, userUuid];
+  const response = await db.query(query, queryParams);
+  const user = response.rows[0];
+  return user;
 };
 
 const uuidToId = async (table, uuid) => {
@@ -108,12 +287,21 @@ const uuidToId = async (table, uuid) => {
 
 module.exports = {
   getEndpointCount,
-  getEvents,
+  getEventCount,
   getEventsByType,
   getEventsByUser,
-  getMessages,
+  getEventTypeCountByEndpoint,
+  getEventTypes,
+  getFailedMessageCount,
+  getFirstMessageDate,
+  getMessageCount,
+  getMessageCountByEndpoint,
   getMessagesByDay,
   getMessagesByMonth,
+  getMessagesByStatus,
+  getMessagesByYear,
+  getService,
+  getUser,
   getUserCount,
   uuidToId,
 };
--- lambdas/stats/index.js
+++ lambdas/stats/index.js
@@ -1,11 +1,64 @@
-"use strict";
-const { getStats, testFunction } = require("./statActions");
+'use strict';
+const { getStats, getEventTypes, getSubscriptions } = require('./statActions');
+const { newResponse, isValidUuid } = require('./utils');
+const db = require('./db');
 
 exports.handler = async (event) => {
-  let { pathParameters } = event;
+  let { pathParameters, queryStringParameters: params } = event;
   const serviceUuid = pathParameters && pathParameters.service_id;
+  const userUuid = params && params.user;
+  console.log(params);
+  if (!isValidUuid(serviceUuid)) {
+    return newResponse(404, {
+      error_type: 'invalid_parameter',
+      detail: 'Service uuid is not a valid uuid',
+    });
+  }
 
-  if (serviceUuid) {
-    return await getStats(serviceUuid);
+  if (userUuid && !isValidUuid(userUuid)) {
+    return newResponse(404, {
+      error_type: 'invalid_parameter',
+      detail: 'User uuid is not a valid uuid',
+    });
+  }
+
+  try {
+    const service = await db.getService(serviceUuid);
+    if (!service) {
+      return newResponse(404, {
+        error_type: 'invalid_parameter',
+        detail: 'Service not found',
+      });
+    }
+
+    if (userUuid) {
+      const user = await db.getUser(service.id, userUuid);
+      if (!user) {
+        return newResponse(404, {
+          error_type: 'invalid_parameter',
+          detail: 'User not found',
+        });
+      }
+
+      switch (params.type) {
+        case 'subscriptions':
+          return await getSubscriptions(user.id);
+      }
+    }
+
+    if (!params) {
+      return await getStats(service.id);
+    }
+
+    switch (params.type) {
+      case 'event_type':
+        return await getEventTypes(service.id);
+    }
+  } catch (error) {
+    console.error(error);
+    return newResponse(500, {
+      error_type: 'process_failed',
+      detail: 'Operation failed',
+    });
   }
 };
